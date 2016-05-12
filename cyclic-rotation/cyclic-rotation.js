55359ec1fd023afe721ab6a732b47016e5ea2a59
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-05-12 16:40:21 +0200

    Finished

--- cyclic-rotation/cyclic-rotation.js
+++ cyclic-rotation/cyclic-rotation.js
@@ -7,25 +7,68 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-rl.question('Please enter a zero-indexed array consisting of integers and an integer K? ', (arr, numOfRotations) => {
-	console.log('');
-	console.log('The length of the longest binary gap: ', getRotatedArray(arr, numOfRotations));
-	console.log('');
+rl.question('Please enter a zero-indexed array consisting of integers, e.g. "1,2,3": ', (arr) => {
+	rl.question('Please enter the number of rotations ? ', (num) => {
+		console.log('');
+		console.log('The rotated array is: ', getRotatedArray(arr, num));
+		console.log('');
 
-	rl.close();
+		rl.close();
+	});
+	
 });
 
-function getRotatedArray (arr, numOfRotations) {
-	if (numOfRotations < 0 || numOfRotations > 100) {
-		console.log("The number of the rotations should be within the range [0..100].");
-		return;
+
+function rotatedArray (arr) {
+	
+	console.log("arr: ", arr);
+
+	var arrRotated = [];
+	var lastEl = arr[arr.length - 1];
+
+	for (var j = 0, len = arr.length; j < len; j++) {
+		if (arr.indexOf(arr[j - 1]) === -1) {
+			arrRotated[j] = lastEl;
+		} else {
+			arrRotated[j] = arr[j - 1];
+		}
 	}
 
-	var i = 0;
-	for (; i < numOfRotations; i++) {
-		arr = arr.map((val, idx) => {
-			console.log("val: ", val);
-			console.log("idx: ", idx);
-		});
+	return arrRotated;
+}
+
+// For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].
+function getRotatedArray (arr, numOfRotations) {
+	// Convert the passed string to array
+	arr = arr.split(',');
+
+	if (arr === 'undefined' || (toString.call(arr) !== '[object Array]') || arr.length < 2) {
+		return "The passed array should be a valid array and should have at least 2 elements";
 	}
+
+
+	if (numOfRotations === 'undefined' || !Number.isInteger(parseInt(numOfRotations)) || numOfRotations < 0 || numOfRotations > 100) {
+		return "The number of the rotations should be within the range [0..100].";
+	}
+
+	for (var i = 0; i < numOfRotations; i++) {
+	 	// arr = rotatedArray(arr);
+	 	arr = function (arr) {
+
+	 		var arrRotated = [];
+	 		var lastEl = arr[arr.length - 1];
+
+	 		for (var j = 0, len = arr.length; j < len; j++) {
+	 			if (arr.indexOf(arr[j - 1]) === -1) {
+	 				arrRotated[j] = lastEl;
+	 			} else {
+	 				arrRotated[j] = arr[j - 1];
+	 			}
+	 		}
+
+	 		return arrRotated;
+	 	}(arr);
+	}
+
+	return arr;
 }
