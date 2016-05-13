f3fd7f7c50560e41a45e3d5fb8c1cb7be0511aa8
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-05-13 14:58:50 +0200

    Finished Odd Occurrences Array. Optimized for better performance with big chunks of data, but still need more

--- odd-occurrences-array/odd-occurrences-array.js
+++ odd-occurrences-array/odd-occurrences-array.js
@@ -7,67 +7,70 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
+var arrLarge = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2];
+
 rl.question('Input: ', (arr) => {
 	console.log('');
-	console.log('Output: ', getUnpairedElement(arr));
+	// console.log('Output: ', getUnpairedElement([9, 3, 9, 3, 9, 7, 9]));
+	// console.log('Output: ', getUnpairedElement(arr));	// Uncomment this is you want to input data via cmd
+	console.log('Output: ', getUnpairedElement(arrLarge));
 	console.log('');
 	rl.close();
 });
 
-
-var arrDuplicates = [];
-function hasDuplicate (el) {
-	if (arrDuplicates.length === 0) {
-		arrDuplicates.push(el);
-		return false;
-	}
-
-	for(var i = 0, len = arrDuplicates.length; i < len; i++){
-		console.log('arrDuplicates[i]: ', arrDuplicates[i]);
-		if (el === arrDuplicates[i]) {
-			return true;
-		} else {
-			arrDuplicates.push(el);
-		}
-	}
-
-	return false;
+function isInt4(value) {
+  return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
 }
 
-
 function getUnpairedElement (arr) {
+	// Uncomment this is you want to input data via cmd
+	/*if (arr) {
+		var arrTemp = arr.split(',');
+		arr = arr.split(',');
+	}*/
+	
 
-	arr = arr.split(',');
-
-	// 1. Verify intpu data - non-empty zero-indexed array containing odd number of integers
+	 // 1. Verify intpu data - non-empty zero-indexed array containing odd number of integers
 	if (arr === 'undefined' || (toString.call(arr) !== '[object Array]') || arr.length === 0 || arr.length % 2 === 0) {
 		return 'The passed array should be a valid non-empty array containing odd number of indegers!';
 	}
 
 	// 2. Verify that the array contains only integers
-	for (var i = 0, len = arr.length; i < len; i++) {
-		if (!Number.isInteger(parseInt(arr[i]))) {
+	var z = 0;
+	while (z < arr.length) {
+		if (!isInt4(arr[z])) {
 			return 'The passed array should be containing only integers';
 		}
+		z++;
 	}
-
-
-	// 3. Find the pairs in the array and remove the duplicates
-	arr = arr.map((el, idx) => {
-		if (arr.length === 1) {
-			return el;
-		}
-
-		if (hasDuplicate(el)) {
-			arr.splice(idx, 1);
-		} else {
-			return el;
-		}
-	});
 	
 
-	console.log('>>> result: ', arr);
+	// 3. Find the number of occurrences for each element
+	var arrTemp = arr.toString().split(','),	// Comment this if you are going to use the console (cmd) to input data
+		arrObjects = [],
+		k = 0,
+		arrLen = arr.length;
 
-	// 4. Return the unpaired element
-	return arr.length === 1 ? arr[0] : 'There is no unpaired element.';
+	while (k < arrLen) {
+		var currEl = arr.splice(0, 1);
+		var obj = { 
+			'occurrences': 0,
+			'elemValue': 0
+		};
+
+		var j = 0;
+		while (j < arrTemp.length) {
+			if (currEl.toString() === arrTemp[j].toString()) {
+				obj.occurrences++;
+				obj.elemValue = arrTemp[j];
+			}
+			j++;
+		}
+		k++;
+
+		
+		if (obj.occurrences === 1) {
+			return parseInt(obj.elemValue, 10);
+		}
+	}
 }
