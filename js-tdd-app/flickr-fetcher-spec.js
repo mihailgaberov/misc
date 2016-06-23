2ca0d5015018f68877c46e04af67889de85152aa
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-06-23 16:36:02 +0200

    Added asynchronous testing and used a stub.

--- js-tdd-app/flickr-fetcher-spec.js
+++ js-tdd-app/flickr-fetcher-spec.js
@@ -85,3 +85,55 @@ describe('#transformPhotoObj()', function() {
         expect(actual).to.eql(expected);
     });
 });
+
+describe('#fetchPhotos()', function() {
+    it('should take an API key and fetcher function, and return a promise for transformed photos', function() {
+        var apiKey   = 'does not matter what this is right now',
+            expected = [{
+                title: 'Dog goes to desperate measure to avoid walking on a leash',
+                url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+            }, {
+                title: 'the other cate',
+                url:   'https://farm2.staticflickr.com/1514/24765033584_3c190c104e_b.jpg'
+            }],
+            fakeData = {
+                'photos': {
+                    'page':    1,
+                    'pages':   2872,
+                    'perpage': 100,
+                    'total':   '287170',
+                    'photo':   [{
+                        id:       '25373736106',
+                        owner:    '99117316@N03',
+                        secret:   '146731fcb7',
+                        server:   '1669',
+                        farm:     2,
+                        title:    'Dog goes to desperate measure to avoid walking on a leash',
+                        ispublic: 1,
+                        isfriend: 0,
+                        isfamily: 0
+                    }, {
+                        id:       '24765033584',
+                        owner:    '27294864@N02',
+                        secret:   '3c190c104e',
+                        server:   '1514',
+                        farm:     2,
+                        title:    'the other cate',
+                        ispublic: 1,
+                        isfriend: 0,
+                        isfamily: 0
+                    }]
+                }
+            },
+            fakeFetcher = function(url) {
+                var expectedURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                            + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+                expect(url).to.equal(expectedURL)
+                return Promise.resolve(fakeData);
+            };
+
+        return FlickrFetcher.fetchPhotos(apiKey, fakeFetcher).then(function(actual) {
+            expect(actual).to.eql(expected);
+        });
+    });
+});
--- js-tdd-app/flickr-fetcher.js
+++ js-tdd-app/flickr-fetcher.js
@@ -4,23 +4,34 @@ var FlickrFetcher;
 
 FlickrFetcher = {
 	photoObjToURL: function (photoObj) {
-		// return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' + photoObj.secret + '_b.jpg';
 		return [
-			'https://farm',
-            photoObj.farm, 
-            '.staticflickr.com/', 
-            photoObj.server, '/',
-            photoObj.id, '_',
-            photoObj.secret, '_b.jpg'
+		'https://farm',
+		photoObj.farm, 
+		'.staticflickr.com/', 
+		photoObj.server, '/',
+		photoObj.id, '_',
+		photoObj.secret, '_b.jpg'
 		].join('');
 	},
 
 	transformPhotoObj: function(photoObj) {
-        return {
-            title: photoObj.title,
-            url:   FlickrFetcher.photoObjToURL(photoObj)
-        };
-    }
+		return {
+			title: photoObj.title,
+			url:   FlickrFetcher.photoObjToURL(photoObj)
+		};
+	},
+
+	fetchFlickrData: function(apiKey, fetch) {
+		var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+		+ apiKey + '&text=pugs&format=json&nojsoncallback=1'
+		return fetch(url);
+	},
+
+	fetchPhotos: function(apiKey, fetch) {
+		return FlickrFetcher.fetchFlickrData(apiKey, fetch).then(function(data) {
+			return data.photos.photo.map(FlickrFetcher.transformPhotoObj);
+		});
+	}
 };
 
 module.exports = FlickrFetcher;
