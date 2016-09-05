0d6602dfb1acdf253f65ec00e90779a45f3919cb
Mihail Gaberov <mihail.gaberov@dreamit.de>
2016-09-05 14:15:57 +0200

    Finished the build script.

--- bash-scripts/build-landing/build_landing_page.sh
+++ bash-scripts/build-landing/build_landing_page.sh
@@ -4,31 +4,39 @@ printf "\n\n\n\n\n\n"
 printf "\n\n========== Build started =========\n\n"
 
 ## Start 'gulp inline' command
+if ! gulp inline; then echo "Running gulp failed."; exit 1; fi
 printf "==> Started gulp inline\n\n"
 
-## Strip the slashes
-if ! sed -i '.bak' 's/\/cms\//cms\//g' *.html; then echo "Stripping the slashes failed."; exit 1; fi
-printf "==> Stripped the slashes\n\n"
+## Wait for gulp to finish
+wait $gulp
 
 ## Create a new folder
-if ! mkdir -p ladningPageBuild; then echo "Creating new landingPageBuild folder failed."; exit 1; fi
-printf "==> Created a new folder 'ladningPageBuild'\n\n"
-
-## Rename build.html to index.html
-if ! mv build.html index.html; then echo "Renaming build.html to index.html failed."; exit 1; fi
-printf "==> Renamed build.html to index.html\n\n"
+if ! mkdir -p landingPageBuild; then echo "Creating new landingPageBuild folder failed."; exit 1; fi
+printf "==> Created a new folder 'landingPageBuild'\n\n"
 
 ## Copy 'cms' forlder and the html to the build folder
-if ! cp -R index.html cms ladningPageBuild/; then echo "Copying the cms folder and the html file to the build folder failed."; exit 1; fi
+if ! cp -r build.html cms landingPageBuild/; then echo "Copying the cms folder and the html file to the build folder failed."; exit 1; fi
 printf "==> Copied the CMS folder content and the html file to the build folder\n\n"
 
-## Clean up
-if ! rm -R index.html build.html.bak; then echo "Cleaning up failed."; exit 1; fi
-printf "==> Clean up files\n\n"
+## Strip the slashes
+if ! sed -i '.bak' 's/\/cms\//cms\//g' landingPageBuild/build.html; then echo "Stripping the slashes failed."; exit 1; fi
+printf "==> Stripped the slashes\n\n"
+
+## Clean up .bak files
+if ! rm -r landingPageBuild/build.html.bak; then echo "Cleaning up failed."; exit 1; fi
+printf "==> Clean up .bak files\n\n"
+
+## Rename build.html to index.html
+if ! mv landingPageBuild/build.html landingPageBuild/index.html; then echo "Renaming build.html to index.html failed."; exit 1; fi
+printf "==> Renamed build.html to index.html\n\n"
 
 ## Create a zip achive to be suitable for sending via email
-if ! zip -R /lp_build.zip ./*; then echo "Creating ZIP failed."; exit 1; fi
-printf "==> Created ZIP achive\n\n"
+if ! zip -r landingPageBuild.zip landingPageBuild/*; then echo "Creating ZIP failed."; exit 1; fi
+printf "==> Created ZIP archive\n\n"
+
+## Clean up
+if ! rm -r landingPageBuild; then echo "Cleaning up failed."; exit 1; fi
+printf "==> Clean up files\n\n"
 
 
-printf "\n\n Build Done Successfully!\n\n\n\n"
+printf "\n\n==- Build Done Successfully! -==\n\n\n\n"
