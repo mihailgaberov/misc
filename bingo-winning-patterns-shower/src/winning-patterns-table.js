3a4ba82be1561d1f1626a60fa43b05e03e7e02db
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-11-22 00:11:46 +0100

    Finished the first version of the horizontal pattern.

--- bingo-winning-patterns-shower/src/winning-patterns-table.js
+++ bingo-winning-patterns-shower/src/winning-patterns-table.js
@@ -13,6 +13,21 @@ class WinningPatternsTable {
 		this.createDomElement();
 	}
 
+	startAnimation() {
+		// Define which pattern animation to start
+		switch (this.pattern) {
+			case 'horizontal':
+				this.startHorizontalAnim();
+				break;
+			case 'vertical':
+				this.startVerticalAnim();
+				break;
+			case 'diagonal':
+				this.startDiagonallAnim();
+				break;
+		}
+	}
+
 	createDomElement() {
 		const elTable = document.createElement('table');
 		let i = 1;
@@ -28,8 +43,45 @@ class WinningPatternsTable {
 			elTable.appendChild(elRow);
 			i++;
 		}
-
 		this.elem.appendChild(elTable);
+
+		this.startAnimation();
+	}
+
+	startHorizontalAnim() {
+		let yIdx = 1;
+		setInterval(() => {
+			// Clear all cells background first
+			const arrCells = document.querySelectorAll('td');
+			let cellIdx = 0;
+			while (cellIdx < arrCells.length) {
+				if (arrCells[cellIdx].classList.contains('highlighted')) {
+					arrCells[cellIdx].classList.remove('highlighted');
+				}
+				cellIdx++;
+			}
+
+			let xIdx = 1;
+			while (xIdx <= this.cols) {
+				const elCell = document.querySelector(`#x${xIdx}y${yIdx}`);
+				elCell.classList.add('highlighted');
+				xIdx++;
+			}
+
+			if (yIdx === this.rows) {
+				yIdx = 0;
+			}
+
+			yIdx++;
+		}, 1000)
+	}
+
+	startVerticalAnim() {
+
+	}
+
+	startDiagonallAnim() {
+
 	}
 }
 
