c20bbe1468fba5f469512c3eaf4400d711bacf78
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-14 16:31:44 +0100

    Update app.ts

--- manage-local-storage-with-typescript/app.ts
+++ manage-local-storage-with-typescript/app.ts
@@ -12,4 +12,10 @@ const generateSettingsKey = (): string => `Settings-00${gen.next().value}`
 localStorage.createRecord(generateSettingsKey(), generateUsername());
 localStorage.createRecord(generateSettingsKey(), generateUsername());
 
-localStorage.clearAll();
+localStorage.deleteRecord('User001');
+localStorage.createRecord(generateSettingsKey(), generateUsername());
+console.log('do we have more: ', localStorage.hasStoredItems());
+console.log('is ls supported: ', localStorage.isLocalStorageSupported());
+localStorage.iterateOverItems();
+localStorage.updateRecord('Settings-000', 'mihail');
+// localStorage.clearAll();
