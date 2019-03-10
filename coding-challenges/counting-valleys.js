4807e8cedc4379639af6ae5b0277bd569c944ab4
mihailgaberov <mihail.gaberov@gmail.com>
2019-03-10 12:50:43 +0100

    Added Counting Valleys challenge.

---
+++ coding-challenges/counting-valleys.js
@@ -0,0 +1,27 @@
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+  let seaLevel = 0;
+  let valleysCount = 0;
+
+
+  for (let idx in s) {
+    if (s.hasOwnProperty(idx)) {
+      const char = s[ idx ]
+
+      if (char === 'D' && seaLevel === 0) {
+        valleysCount++
+      }
+
+      if (char === 'U') {
+        seaLevel++
+      } else {
+        seaLevel--
+      }
+    }
+  }
+
+  return valleysCount
+}
+
+console.log(countingValleys(8, 'UDDDUDUU')) // 1
+console.log(countingValleys(12, 'DDUUDDUDUUUD')) // 2
