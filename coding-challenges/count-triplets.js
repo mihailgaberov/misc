17f86081bdb98a8d50628339cf8c5f18fc9b8ee0
mihailgaberov <mihail.gaberov@gmail.com>
2019-03-17 09:01:55 +0100

    Added memoized version of Count Triplets challenge.

--- coding-challenges/count-triplets.js
+++ coding-challenges/count-triplets.js
@@ -2,10 +2,25 @@
 
 For example, arr=[1,4,16,64]. If r=4, we have [1,4,16] and [4,16,64] at indices (0,1,2) and (1,2,3).
 */
-
-function isGeomProgression(i, j, k, ratio) {
-  return (i * ratio === j && j * ratio === k)
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    const inputs = [args[0], args[1], args[2], args[3]]
+    if (inputs in cache) {
+      // console.log('Fetching from cache', inputs);
+      return cache[inputs];
+    } else {
+      // console.log('Calculating result', inputs);
+      let result = fn(inputs);
+      cache[inputs] = result;
+      return result;
+    }
+  }
 }
+
+const isGeomProgression = memoize(function ([i, j, k, ratio]) {
+  return (i * ratio === j && j * ratio === k)
+})
 
 function countTriplets(arr, r) {
   let l = 0
@@ -19,9 +34,9 @@ function countTriplets(arr, r) {
       const second = arrRest[m]
       const arrRestSecond = arrRest.slice(m + 1)
       let n = 0
-      while(n < arrRestSecond.length) {
+      while (n < arrRestSecond.length) {
         const third = arrRestSecond[n]
-        count += isGeomProgression(first,second,third,r)
+        count += isGeomProgression(first, second, third, r)
         n++
       }
       m++
@@ -35,8 +50,8 @@ function countTriplets(arr, r) {
 
 
 console.log(countTriplets([1, 4, 16, 64], 4)) // 2
-console.log(countTriplets([ 1, 2, 2, 4 ], 2)) // 2
+console.log(countTriplets([1, 2, 2, 4], 2)) // 2
 console.log(countTriplets([1, 3, 9, 9, 27, 81], 3)) // 6
 console.log(countTriplets([1, 5, 5, 25, 125], 5)) // 4
-// console.log(countTriplets([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1)) // 161700
-console.log(countTriplets([1,1,1,1],1)) // 4
+console.log(countTriplets([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1)) // 161700
+console.log(countTriplets([1, 1, 1, 1], 1)) // 4
