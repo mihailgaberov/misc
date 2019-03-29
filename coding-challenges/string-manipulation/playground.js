01547974597f301985410ce3dd832ace6a937ffa
mihailgaberov <mihail.gaberov@gmail.com>
2019-03-29 11:04:54 +0100

    Solving Palindrome Permutations challenge - replaced modulus operator with bitwise AND.

---
+++ coding-challenges/string-manipulation/playground.js
@@ -0,0 +1,31 @@
+const numbers = [1,2,3,4,5,6]
+let iterator = numbers[Symbol.iterator]();
+console.log(iterator.next().value); // 1
+console.log(iterator.next().value); // 2
+console.log(iterator.next().value); // 3
+// console.log(iterator.next().value); // 4
+// console.log(iterator.next().value); // 5
+// console.log(iterator.next().value); // 6
+
+for (const n of iterator) {
+  console.log('for of: ',n);
+}
+
+let aEntries = numbers.entries(); // retrieve iterator of key/value
+console.log(aEntries.next().value); // [0, 1] - position 0, value 1
+console.log(aEntries.next().value); // [1, 2] - position 1, value 2
+console.log(aEntries.next().value); // [2, 3] - position 2, value 3
+
+for (const n of aEntries) {
+  console.log(n);
+}
+
+const aKeys = numbers.keys(); // retrieve iterator of keys
+console.log(aKeys.next()); // {value: 0, done: false }
+console.log(aKeys.next()); // {value: 1, done: false }
+console.log(aKeys.next()); // {value: 2, done: false }
+
+const aValues = numbers.values();
+console.log(aValues.next()); // {value: 1, done: false }
+console.log(aValues.next()); // {value: 2, done: false }
+console.log(aValues.next()); // {value: 3, done: false }
