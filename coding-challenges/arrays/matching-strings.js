745f14cb8b8210fb735d928a516f76d27f8738db
Mihail Gaberov <mihail.gaberov@williamhill.com>
2019-10-02 16:35:44 +0200

    Added matching-strings task.

---
+++ coding-challenges/arrays/matching-strings.js
@@ -0,0 +1,52 @@
+const sampleInputStrings = ['aba', 'baba', 'aba', 'xzxb']
+const sampleInputQueries = ['aba', 'xzxb', 'ab']
+const sampleInputStrings1 = ['def', 'de' , 'fgh']
+const sampleInputQueries1 = ['de', 'lmn', 'fgh']
+const sampleInputStrings2 = ['abcde',
+    'sdaklfj',
+    'asdjf',
+    'na',
+    'basdn',
+    'sdaklfj',
+    'asdjf',
+    'na',
+    'asdjf',
+    'na',
+    'basdn',
+    'sdaklfj',
+    'asdjf'
+]
+const sampleInputQueries2 = ['abcde',
+    'sdaklfj',
+    'asdjf',
+    'na',
+    'basdn']
+
+function matchingStrings(strings, queries) {
+    const stringsMap = {}
+
+    strings.forEach(el => {
+        if (stringsMap[el]) {
+            stringsMap[el]++   
+        } else {
+            stringsMap[el] = 1
+        }
+    });
+
+    console.log('map: ', stringsMap)
+
+    const res = []
+    queries.forEach(el => {
+        if (Object.keys(stringsMap).includes(el)) {
+            res.push(stringsMap[el])
+        } else {
+            res.push(0)
+        }
+    })
+
+    return res
+}
+
+console.log(matchingStrings(sampleInputStrings, sampleInputQueries)) // 2,1,0
+console.log(matchingStrings(sampleInputStrings1, sampleInputQueries1)) // 1 0 1
+console.log(matchingStrings(sampleInputStrings2, sampleInputQueries2)) // 1 3 4 3 2
