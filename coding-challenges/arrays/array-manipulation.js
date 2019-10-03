198c39b57ab29575a1f37c2a551a032aaba09562
Mihail Gaberov <mihail.gaberov@williamhill.com>
2019-10-03 11:17:17 +0200

    Added helper for converting long string to an array of array with 3 elements.

--- coding-challenges/arrays/array-manipulation.js
+++ coding-challenges/arrays/array-manipulation.js
@@ -1,11 +1,14 @@
 const arrSize = 5 // Starting with a 1-indexed array of zeros, the number of elements in your array
-
 const queries = [ [ 1, 2, 100 ], [ 2, 5, 100 ], [ 3, 4, 100 ] ] // a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
 
+const largeInput = 1000000
+const largeQueries = [
+    
+]
 
 function arrayManipulation(n, queries) {
     const arr = new Array(n).fill(0);
-    
+    console.time('st')
     queries.forEach(el => {
         const leftIdx = el[0]
         const rightIdx = el[1]
@@ -14,11 +17,12 @@ function arrayManipulation(n, queries) {
         for (let i = leftIdx - 1; i <= rightIdx - 1; i++) {
             arr[i] += summand
         }
-
-        console.log(arr)
     });
-
-    return Math.max(...arr)
+    
+    // return Math.max(...arr)
+    const res = Math.max(...arr)
+    console.timeEnd('st')
+    return res
 }
 
 console.log(arrayManipulation(arrSize, queries)) // 200
